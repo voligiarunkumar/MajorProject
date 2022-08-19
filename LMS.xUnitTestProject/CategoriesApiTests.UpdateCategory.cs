4b55f5ff3735daad53acf4888a81b06c934b1f76
arunkumar <voligiarunkumar9912345@gmail.com>
2022-08-19 21:47:39 +0530

    Add project files.

---
+++ LMS.xUnitTestProject/CategoriesApiTests.UpdateCategory.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+using Xunit.Abstractions;
+using Moq;
+using Microsoft.Extensions.Logging;
+using LMS.Web.Controllers;
+using LMS.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+
+
+namespace LMS.xUnitTestProject
+{
+    /// <remarks>
+    ///     Bad update data scenarios:
+    ///     - Name is NULL
+    ///     - Name is EMPTY STRING
+    ///     - Name contains more characters than what is allowed
+    ///     - NULL Category object
+    ///     - ID not matching with the ID of the row identified to be updated.
+    ///     - ID replaced with a negative value
+    ///     - Replacing the object retrieved before the update, with a completely new object
+    ///     - Since the HTTP PUT receives a Nullable INT as first parameter, pass a NULL value
+    ///
+    ///     LIMITATIONS OF WORKING WITH InMemory Database
+    ///     - Relationships between Tables are not supported.
+    ///     - EF Core DataAnnotation Validations are not supported.
+    /// </remarks>
+
+    public partial class CategoriesApiTests
+    {
+        [Fact]
+        public async void UpdateCategory_OkResult01()
+        {
+            // ARRANGE
+            var dbName = nameof(CategoriesApiTests.UpdateCategory_OkResult01);
+            var logger = Mock.Of<ILogger<CategoriesController>>();
+            using var dbContext = DbContextMocker.GetApplicationDbContext(dbName);      // Disposable!
+
+            var controller = new CategoriesController(dbContext, logger);
+            int editCategoryId = 2;
+            Category originalCategory, changedCategory;
+            changedCategory = new Category
+            {
+                CategoryId = editCategoryId,
+                CategoryName = "--Changed Category Name"
+            };
+
+            // ACT #1: Get the Record to Edit.
+
+            // (a) Get the Category to edit (to ensure that the row exists before editing it)
+            IActionResult actionResultGet = await controller.GetCategory(editCategoryId);
+            
+            // (b) Check if HTTP 200 "Ok" 
+            OkObjectResult OkResult = actionResultGet.Should().BeOfType<OkObjectResult>().Subject;
+
+            // (c) Extract the Category Object from the OkObjectResult
+            originalCategory = OkResult.Value.Should().BeAssignableTo<Category>().Subject;
+
+            // (d) Check if the data to be edited was received from the API
+            Assert.NotNull(originalCategory);
+
+            _testOutputHelper.WriteLine("Retrieved the Data from the API.");
+
+            try
+            {
+                // ACT #2: Try to update the data, using a completely new object
+                //         NOTE: This will throw the InvalidOperationException!
+                var actionResultPutAttempt1 = await controller.PutCategory(editCategoryId, changedCategory);
+
+                // ASSERT - if the update was successfull.
+                Assert.IsType<OkResult>(actionResultPutAttempt1);
+
+                _testOutputHelper.WriteLine("Updated the changes back to the API - using a new object.");
+            }
+            catch (System.InvalidOperationException exp)
+            {
+                // The PUT operation should throw this exception,
+                // because it is an object that does not carry change tracking information.
+
+                _testOutputHelper.WriteLine("Failed to update the change back to the API - using a new object");
+                _testOutputHelper.WriteLine($"-- Exception Type: {exp.GetType()}");
+                _testOutputHelper.WriteLine($"-- Exception Message: {exp.Message}");
+                _testOutputHelper.WriteLine($"-- Exception Source: {exp.Source}");
+                _testOutputHelper.WriteLine($"-- Exception TargetSite: {exp.TargetSite}");
+            }
+        }
+
+        [Fact]
+        public async void UpdateCategory_OkResult02()
+        {
+            // ARRANGE
+            var dbName = nameof(CategoriesApiTests.UpdateCategory_OkResult02);
+            var logger = Mock.Of<ILogger<CategoriesController>>();
+            using var dbContext = DbContextMocker.GetApplicationDbContext(dbName);      // Disposable!
+
+            var controller = new CategoriesController(dbContext, logger);
+            int editCategoryId = 2;
+            Category originalCategory;
+            string changedCategoryName = "--- CHANGED Category Name";
+
+            // ACT #1: Get the Record to Edit.
+
+            // (a) Get the Category to edit (to ensure that the row exists before editing it)
+            IActionResult actionResultGet = await controller.GetCategory(editCategoryId);
+
+            // (b) Check if HTTP 200 "Ok" 
+            OkObjectResult OkResult = actionResultGet.Should().BeOfType<OkObjectResult>().Subject;
+
+            // (c) Extract the Category Object from the OkObjectResult
+            originalCategory = OkResult.Value.Should().BeAssignableTo<Category>().Subject;
+
+            // (d) Check if the data to be edited was received from the API
+            Assert.NotNull(originalCategory);
+
+            _testOutputHelper.WriteLine("Retrieved the Data from the API.");
+
+            // ACT #2: Now, try to update the data
+            // SOLUTION: The following lines would work!
+            //           Reason, we are modifying the data originally received.
+            //           And then, calling the PUT operation.
+            //           So, the API is able to find the ChangeTracking data associated to the object.
+
+            // (a) Change the data of the object that was received from the API.
+            originalCategory.CategoryName = changedCategoryName;
+
+            // (b) Call the HTTP PUT API to update the changes (with the updated object)
+            var actionResultPutAttempt2 = await controller.PutCategory(editCategoryId, originalCategory);
+
+            // ASSERT - if the update was successfull.
+            Assert.IsType<NoContentResult>(actionResultPutAttempt2);
+
+            _testOutputHelper.WriteLine("Updated the changes back to the API - using the object received");
+        }
+
+    }
+} 
